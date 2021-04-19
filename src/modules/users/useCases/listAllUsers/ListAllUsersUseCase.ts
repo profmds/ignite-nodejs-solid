efe066d805338a231ed742b96696c06dd1cf3fe4
Marcelo dos Santos <profmds@gmail.com>
2021-04-18 23:59:42 -0300

    feat: implementation of the execute method in ListAllUsersUseCase

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,12 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userList = this.usersRepository.list();
+
+    return userList;
   }
 }
 
