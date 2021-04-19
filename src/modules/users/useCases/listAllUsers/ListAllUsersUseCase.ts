972ecc1f5fada34e4b747362b1158a136e662e9a
Marcelo dos Santos <profmds@gmail.com>
2021-04-19 00:02:04 -0300

    feat: return an error if a non existing user call List All Users

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,6 +9,11 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
+    const findUser = this.usersRepository.findById(user_id);
+
+    if (!findUser) {
+      throw new Error("User not found!");
+    }
     const userList = this.usersRepository.list();
 
     return userList;
